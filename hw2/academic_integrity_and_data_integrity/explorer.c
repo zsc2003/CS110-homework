bfa3d4b6c7d2c613af879d4b6a8004a6b64cc651
zsc2003 <zhoushch@shanghaitech.edu.cn>
2023-02-24 21:23:13 +0800

    done

--- hw2/academic_integrity_and_data_integrity/explorer.c
+++ hw2/academic_integrity_and_data_integrity/explorer.c
@@ -4,7 +4,8 @@
 #include "node.h"
 #include <stdlib.h>
 #include <string.h>
-
+// headers
+// functions
 struct explorer *explorer_new(void) {
   // Allocate memory
   struct explorer *exp = calloc(1, sizeof(struct explorer));
@@ -50,7 +51,7 @@ bool explorer_write(struct explorer *exp, const char *name, int offset,
   }
   // Check if the file exisits
   sub = dir_find_node(exp->cwd, name);
-  if (!sub || sub->is_dir) {
+  if (!sub || sub->is_dir) { // handle null pointer 
     return false;
   }
   // Write the file
@@ -68,7 +69,7 @@ bool explorer_create(struct explorer *exp, char *name, int type) {
 
 bool explorer_mkdir(struct explorer *exp, char *name) {
   // Check for valid arguments
-  if (!exp || dir_find_node(exp->cwd, name)) {
+  if (!exp || dir_find_node(exp->cwd, name)) { // handle null pointer
     return false;
   }
   // Add subdir
@@ -85,8 +86,8 @@ bool explorer_delete(struct explorer *exp, const char *name) {
 }
 
 bool explorer_cdpar(struct explorer *exp) {
-  if (!exp || exp->cwd == exp->root) {
-    return false;
+  if (!exp || exp->cwd == exp->root) { // handle null pointer
+    return false;// check not root
   }
   exp->cwd = exp->cwd->parent;
   return true;
@@ -94,79 +95,79 @@ bool explorer_cdpar(struct explorer *exp) {
 
 bool explorer_chdir(struct explorer *exp, const char *name) {
   struct node *sub = NULL;
-  if (!exp || !name) {
+  if (!exp || !name) {// handle null pointer
     return false;
   }
   sub = dir_find_node(exp->cwd, name);
   if (!sub || !sub->is_dir) {
-    return false;
+    return false;// handle null pointer
   }
   exp->cwd = sub->inner.dir;
-  return true;
+  return true;//success
 }
 
 bool explorer_support_filetype(struct explorer *exp, open_func callback,
                                int filetype) {
   if (!exp || callback == NULL || filetype < 0 || filetype >= MAX_FT_NO) {
+    return false;// handle null pointer
+  }
+  if (exp->ft_open[filetype] != NULL) {// handle null pointer
     return false;
   }
-  if (exp->ft_open[filetype] != NULL) {
-    return false;
-  }
-  exp->ft_open[filetype] = callback;
+  exp->ft_open[filetype] = callback;//success
   return true;
 }
 
 bool explorer_open(const struct explorer *exp, const char *name) {
-  struct node *sub = NULL;
+  struct node *sub = NULL;//initialize
   struct file *file = NULL;
-  if (!exp || name == NULL) {
+  if (!exp || name == NULL) {// handle null pointer
     return false;
   }
   sub = dir_find_node(exp->cwd, name);
-  if (!sub || sub->is_dir) {
+  if (!sub || sub->is_dir) {// handle null pointer
     return false;
   }
   file = sub->inner.file;
   if (!exp->ft_open[file->type]) {
-    return false;
+    return false;// handle null pointer
   }
   exp->ft_open[file->type](file);
-  return true;
+  return true;//success
 }
 
 static void search(struct node *node, char **path, const char *name,
                    find_func callback) {
-  struct directory *dir = NODE_DIR(node);
+  struct directory *dir = NODE_DIR(node);// make directory
   int i = 0, pathlen = strlen(*path);
 
   *path = realloc(*path, pathlen + strlen(node->name) + 2);
-  strcat(*path, "/");
+  strcat(*path, "/"); //make path
   strcat(*path, node->name);
 
   if (node->is_dir) {
-    for (i = 0; i < dir->size; i++) {
+    for (i = 0; i < dir->size; i++) { // search directory
       search(dir->subordinates[i], path, name, callback);
     }
   } else if (strncmp(node->name, name, MAX_NAME_LEN) == 0) {
-    callback(*path, NODE_FILE(node));
+    callback(*path, NODE_FILE(node)); // get the directory
   }
 
-  *path = realloc(*path, pathlen + 1);
+  *path = realloc(*path, pathlen + 1); // enlarge path
   (*path)[pathlen] = '\0';
 }
 
 void explorer_search_recursive(struct explorer *exp, const char *name,
                                find_func callback) {
-  char *path = NULL;
-  if (!exp || !name || !callback) {
+  char *path = NULL; // initialize
+  if (!exp || !name || !callback) {// handle null pointer
     return;
   }
   path = calloc(1, 1);
-  search(exp->cwd->base, &path, name, callback);
-  free(path);
+  search(exp->cwd->base, &path, name, callback); 
+  free(path); // free memory
 }
 
 bool explorer_contain(struct explorer *exp, const char *name) {
-  return exp ? dir_find_node(exp->cwd, name) != NULL : false;
+  return exp ? dir_find_node(exp->cwd, name) != NULL : false; // check contain
 }
